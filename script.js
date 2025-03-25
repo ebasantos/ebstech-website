e5a853eea8ef63f0fc6055941d6bc27f34faf2e6
Erik Santos <42896599+ebasantos@users.noreply.github.com>
2025-03-25 15:24:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Função para animar a rolagem suave ao clicar nos links do menu
+const scrollLinks = document.querySelectorAll('nav a');
+
+scrollLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = e.target.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop - 80,
+            behavior: 'smooth'
+        });
+    });
+});
